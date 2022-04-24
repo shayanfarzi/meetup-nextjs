7140be221487b98596f87b3b93e8a7f19ef50416
shayanfarzi <farzishayan@gmail.com>
2022-04-24 17:55:42 +0430

    layout

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,10 +1,13 @@
-import MainNavigation from './MainNavigation';
-import classes from './Layout.module.css';
-
+import MainNavigation from "./MainNavigation";
+import classes from "./Layout.module.css";
+// import { useRouter } from "next/router";
 function Layout(props) {
+  // const router = useRouter();
+
   return (
     <div>
       <MainNavigation />
+      {/* {router.pathname.search("meetup") > 0 && <div>Hello</div>} */}
       <main className={classes.main}>{props.children}</main>
     </div>
   );
